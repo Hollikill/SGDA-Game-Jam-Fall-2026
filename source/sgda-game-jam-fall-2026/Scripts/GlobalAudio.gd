a21da853516bdb16d56499d1b6bff25975011ae1
FireDrake987 <danielmlouk@gmail.com>
2026-09-21 18:30:28 -0500

    Added sfx options in code Added audio busses Added GlobalAudio support for volume change

--- source/sgda-game-jam-fall-2026/Scripts/GlobalAudio.gd
+++ source/sgda-game-jam-fall-2026/Scripts/GlobalAudio.gd
@@ -1,9 +1,13 @@
 extends Node
 
 @onready var player: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var sfx: AudioStreamPlayer = AudioStreamPlayer.new()
 
 var tracks: Dictionary = {
 	"1": preload("res://Resources/Audio/1.mp3")
+}
+
+const sounds = {
 }
 
 func _ready() -> void:
@@ -17,3 +21,26 @@ func swap_track(track_name: String) -> void:
 	var current_position: float = player.get_playback_position()
 	player.stream = tracks[track_name]
 	player.play(current_position)
+
+func play_sfx(sound_name: String):
+	if not sounds.has(sound_name):
+		return
+	for player in get_children():
+		if not player.playing:
+			player.stream = sounds[sound_name]
+			player.bus = "SFX"
+			player.play()
+			return
+	var new_player = AudioStreamPlayer.new()
+	add_child(new_player)
+	new_player.stream = sounds[sound_name]
+	new_player.bus = "SFX"
+	new_player.play()
+
+func changeAudioVolume(master: float, sfx: float, music: float): 
+	var master_bus = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(master_bus, linear_to_db(master)) 
+	var sfx_bus = AudioServer.get_bus_index("SFX")
+	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(sfx)) 
+	var music_bus = AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_volume_db(music_bus, linear_to_db(music)) 
